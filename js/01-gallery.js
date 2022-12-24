53ba7af0d0c41fa1ef587037388215cab0adb68d
NinaSlominska <nusya.slominskaya@gmail.com>
2022-12-24 23:24:25 +0200

    gallery1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery')
+const markupArray = galleryItems.map(({ preview, original, description }) =>
+{
+   
+   return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+       src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`})
 
-console.log(galleryItems);
+const markup = markupArray.join('')
+
+gallery.insertAdjacentHTML("beforeend", markup)
+gallery.addEventListener('click', galleryClick)
+
+function galleryClick(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`)  
+    instance.show()
+
+}
+
+
+
+
+
+
+{/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */}
